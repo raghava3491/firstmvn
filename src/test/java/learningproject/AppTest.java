c6de53c65180d41271fc0679a7c839d6f03fdf67
Raghavendra Reddy Tippana <raghavendrareddytippana@raghavendrasmbp.lan>
2024-07-15 13:41:39 +0800

    sample java project created by maven

---
+++ src/test/java/learningproject/AppTest.java
@@ -0,0 +1,20 @@
+package learningproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
